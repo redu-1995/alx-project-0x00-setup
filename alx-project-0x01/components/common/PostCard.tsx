744610a656ca502e5b5fb96030e69b22c510cf12
Rediet Abreham <nara1415karol@gmail.com>
2025-06-02 16:43:20 +0300

    adding contents to files

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,5 @@
+const PostCard: React.FC = () => {
+  return <div className="p-4 border rounded shadow">Post Content</div>;
+};
+
+export default PostCard;
---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,6 @@
+const Footer: React.FC = () => {
+  return <footer className="bg-gray-800 text-white p-4 mt-4">Footer</footer>;
+};
+
+export default Footer;
+
